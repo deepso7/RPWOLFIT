c491ebc87bd724a08e2f0a29cf3b75aef0a55f11
deeps <deeps.o7o8.y@gmail.com>
2020-09-08 09:38:03 +0530

    Change app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const ejs = require("ejs");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 const supplyData = [
